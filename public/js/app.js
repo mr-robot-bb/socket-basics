6dc201116379521b90e45f8fae41a62d2a4582e2
Henderikus Top <henderikus.top@gmail.com>
2016-01-18 21:06:59 +0100

    show timestamp on ui

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,8 @@ socket.on('connect', function(){
 socket.on('message', function(message){
 	console.log('New message');
 	console.log(message.text);
-	jQuery('.messages').append('<p>' + message.text + '</p>');
+	var timestampMoment = moment.utc(message.timestamp);
+	jQuery('.messages').append('<p>' + timestampMoment.local().format("h:mma") + ' ' + message.text + '</p>');
 })
 
 // Handles submitting of new message
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var moment = require('moment');
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connect', function(socket){
@@ -11,11 +13,13 @@ io.on('connect', function(socket){
 
 	socket.on('message', function(message){
 		console.log('Message received: ' + message.text);
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the chat application!'
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
 	});
 
 });
